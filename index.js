a1bff95932411785a90ce4555ce61d978c403bb3
ajayip98 <ajayipnurberdiyeva98@gmail.com>
2021-07-26 21:14:04 +0300

    Exercise blog post and price range

--- index.js
+++ index.js
@@ -31,4 +31,37 @@
  function areSame(address1, address2) {
     return address1 === address2;
      }
-    
+    // Exercise - Blog post
+const blogPost = {
+    title: "The wonderful world",
+    body: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Consectetur, deleniti incidunt itaque deserunt odio nihil ipsa illo ducimus",
+    author: "John Adams",
+    views: 1.345,
+    comments: [
+      { author: "a", body: "b" },
+      { author: "a", body: "b" },
+    ],
+    isLive: true,
+  };
+  
+  console.log(blogPost);
+  
+  function Post(title, body, author) {
+    this.title = title;
+    this.body = body;
+    this.author = author;
+    this.views = 0;
+    this.comments = [];
+    this.isLive = false;
+  }
+  
+  const post = new Post("a", "b", "c");
+  console.log(post);
+  
+  // Exercise - Price range;
+  const priceRange = [
+    {Label: '$', tooltip: 'Inexpensive', minPerPerson: 0, maxPerPerson: 10},
+    {Label: '$$', tooltip: 'Moderate', minPerPerson: 11, maxPerPerson: 20},
+    {Label: '$$$', tooltip: 'Expensive', minPerPerson: 21, maxPerPerson: 50}
+  ]
+  
